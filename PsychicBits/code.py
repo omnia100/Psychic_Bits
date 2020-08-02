05788b81ae261c8fff86c2dd5a65d9acf647d6bc
omnia <oomniaa.2020@gmail.com>
2020-08-02 19:47:53 +0200

    add the schedule to DB

---
+++ PsychicBits/code.py
@@ -0,0 +1,17 @@
+import sys, os, django
+sys.path.append(r"C:\Users\Omnia\Documents\GitHub\pbProject")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pbProject.settings")
+django.setup()
+
+import csv
+import os
+from PsychicBits.models import Match
+
+path = r'C:\\Users\Omnia\Desktop'
+os.chdir(path)
+
+with open('E0.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        p = Match(date=row['Date'],HT=row['HomeTeam'], AT=row['AwayTeam'])
+        p.save()
