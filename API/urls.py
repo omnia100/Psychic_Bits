b60d86bf355f220cb5835d8c59d00ec562613443
AmiraMosa <Amaam.6952@gmail.com>
2020-08-04 23:59:22 +0200

    Update urls.py

--- API/urls.py
+++ API/urls.py
@@ -3,11 +3,16 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
+<<<<<<< Updated upstream
  
  #path('match/<int:pk>', views.matchList),
  path('match/<HomeTeam>/<AwayTeam>/', views.searchMatch),
  
+=======
+>>>>>>> Stashed changes
 
+ path('match/<HomeTeam>/<AwayTeam>/', views.searchMatch),
+ 
 
 ]
 
