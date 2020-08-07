9e2eb1a1d5f597c324aaf34b7a5b53fa00af43ed
omnia <oomniaa.2020@gmail.com>
2020-08-07 18:05:29 +0200

    user forms

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User, UserManager
 
-# Create your models here.
+
+# user: username, email, password
+# user optionally has a profile
+
+class Profile(models.Model):
+    objects = UserManager
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    photo = models.ImageField(blank=True)
+    score = models.IntegerField(default=0)
+
