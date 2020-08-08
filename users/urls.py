ae3938f4a6c6605a5e29ab835a1f596cb8e2f87e
AmiraMosa <Amaam.6952@gmail.com>
2020-08-08 15:02:26 +0200

    delete user

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('logout/', views.logout, name='logout'),
-    path('login/', views.log_in, name='login'),
-    path('<user_name>/', views.profile, name='profile'),
+
 ]
