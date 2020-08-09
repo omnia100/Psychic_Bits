fca032ac54d4cf38c004dd3458389a27f76fb782
AmiraMosa <Amaam.6952@gmail.com>
2020-08-10 01:00:10 +0200

    modify user prediction

--- userPrediction/urls.py
+++ userPrediction/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [ 
  #path('match/<int:pk>', views.matchList),
  path('prediction/<int:pk>', views.showPrediction),
- path('vote/<int:userID>/<int:matchID>/<str:voting>/', views.vote),
+ path('vote/<user_name>/<int:matchID>/<str:voting>/', views.vote),
  path('score/<int:matchID>/<str:result>/', views.calculateScore),
 
 ]
--- userPrediction/views.py
+++ userPrediction/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import prediction
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -8,6 +8,7 @@ from . serializers import predictionSerializer
 from . models import prediction
 from PsychicBits.models import Match
 from users.models import Profile
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -35,44 +36,35 @@ def showPrediction(request,pk):
 
 
 @login_required(login_url='psychicbits/mainhome.html')
-def vote(request,userID,matchID,voting):
+def vote(request,user_name,matchID,voting):
 	
 	match=Match.objects.get(pk=matchID)
-	newVote=prediction.objects.create(matchID=match ,vote=voting)
+	user = get_object_or_404(User, username=user_name)
+	profileObj=get_object_or_404(Profile, user=user)
+	newVote=prediction.objects.create(userID=profileObj,matchID=match ,vote=voting)
 	
-	#get messag vot is done 
-	#you have to log in
 
-	return HttpResponse('')
+	return redirect('/psychicbits/mainhome')
+
+
+
+
+
 
 
 def calculateScore(request, matchID, result):
 	match=Match.objects.get(pk=matchID)
-
 	truePredictions=prediction.objects.filter(matchID__id=matchID).filter(vote=result)#return queryset of matchID res filtered prediction 
 
-	#print(truePredictions)
-	
-
-	print(truePredictions)
 	if not truePredictions:
 		return HttpResponse('<h1>No true prediction</h1>')
 
-	
 	for predictionObj in truePredictions: 
 		predictor=Profile.objects.get(pk=predictionObj.userID)
 		predictor.score+=1
 		predictor.save()
 	return HttpResponse('score increased')
 	
-	
-
-
-		
-"""
-		
-
-"""
 	
 
 	
